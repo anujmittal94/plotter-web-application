85d0e00af384c26f1965c70d4fc0067ea09aa37f
anujmittal94 <41164277+anujmittal94@users.noreply.github.com>
2020-12-05 16:32:54 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'adminuser'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'devthrowawaykey'
     MAX_CONTENT_LENGTH = 1024 * 1024
     UPLOAD_PATH = 'uploads'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
